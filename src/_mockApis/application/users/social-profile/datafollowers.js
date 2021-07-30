13d64e06d62375fa4b747f99f261520e03d468b3
{eltr.kc} <{trandinhhiep789@gmail.com}>
2021-07-30 15:59:37 +0700

    setup data followers

---
+++ src/_mockApis/application/users/social-profile/datafollowers.js
@@ -0,0 +1,140 @@
+import follower1 from '../../../../assets/images/Application/User/SocialProfile/Follower/follower1.png';
+import follower2 from '../../../../assets/images/Application/User/SocialProfile/Follower/follower2.png';
+import follower3 from '../../../../assets/images/Application/User/SocialProfile/Follower/follower3.png';
+import follower4 from '../../../../assets/images/Application/User/SocialProfile/Follower/follower4.png';
+import follower5 from '../../../../assets/images/Application/User/SocialProfile/Follower/follower5.png';
+import follower6 from '../../../../assets/images/Application/User/SocialProfile/Follower/follower6.png';
+import follower7 from '../../../../assets/images/Application/User/SocialProfile/Follower/follower7.png';
+import follower8 from '../../../../assets/images/Application/User/SocialProfile/Follower/follower8.png';
+import follower9 from '../../../../assets/images/Application/User/SocialProfile/Follower/follower9.png';
+import follower10 from '../../../../assets/images/Application/User/SocialProfile/Follower/follower10.png';
+import follower11 from '../../../../assets/images/Application/User/SocialProfile/Follower/follower11.png';
+
+export const datafollowers = [
+    {
+        name: 'Barney',
+        adress: 'Handburgh',
+        status: 'true',
+        avatar: follower1
+    },
+    {
+        name: 'Thea',
+        adress: 'New jana',
+        status: 'false',
+        avatar: follower2
+    },
+    {
+        name: 'Guiseppe',
+        adress: 'Jenkinsstad',
+        status: 'true',
+        avatar: follower3
+    },
+    {
+        name: 'Henderson',
+        adress: 'South Antoni',
+        status: 'true',
+        avatar: follower4
+    },
+    {
+        name: 'Anthony',
+        adress: 'Lake Judy',
+        status: 'true',
+        avatar: follower5
+    },
+    {
+        name: 'Reggie',
+        adress: 'Kailynland',
+        status: 'true',
+        avatar: follower6
+    },
+    {
+        name: 'Francesca',
+        adress: 'Pagacview',
+        status: 'false',
+        avatar: follower7
+    },
+    {
+        name: 'Carmel',
+        adress: 'Port Lerashire',
+        status: 'true',
+        avatar: follower8
+    },
+    {
+        name: 'Darwin',
+        adress: 'North Jacqul',
+        status: 'false',
+        avatar: follower9
+    },
+    {
+        name: 'Kaylin',
+        adress: 'Bergstrombuhaha',
+        status: 'true',
+        avatar: follower10
+    },
+    {
+        name: 'Kamryn',
+        adress: 'South Norma',
+        status: 'true',
+        avatar: follower11
+    },
+    {
+        name: 'Madelyn',
+        adress: 'Port Opheliamouth',
+        status: 'true',
+        avatar: follower1
+    },
+    {
+        name: 'Guiseppe',
+        adress: 'Handburgh',
+        status: 'true',
+        avatar: follower2
+    },
+    {
+        name: 'Hayden',
+        adress: 'Darrelshaire',
+        status: 'true',
+        avatar: follower3
+    },
+    {
+        name: 'Kyler',
+        adress: 'Murraymouth',
+        status: 'true',
+        avatar: follower4
+    },
+    {
+        name: 'Pamela',
+        adress: 'Murraymouth',
+        status: 'true',
+        avatar: follower5
+    },
+    {
+        name: 'Reggie',
+        adress: 'Kailynland',
+        status: 'true',
+        avatar: follower6
+    },
+    {
+        name: 'Anthony',
+        adress: 'Lake Judy',
+        status: 'true',
+        avatar: follower7
+    },
+    {
+        name: 'Darwin',
+        adress: 'Pagacview',
+        status: 'true',
+        avatar: follower8
+    },
+    {
+        name: 'Betty',
+        adress: 'Bergstrombury',
+        status: 'true',
+        avatar: follower9
+    },
+    {
+        name: 'Henderson',
+        adress: 'South Antoni',
+        status: 'true',
+        avatar: follower10
+    }
+];
